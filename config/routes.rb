1303ca71b6d81ec592ac75985cf225f528c8a25c
Ela <teodocio.am@gmail.com>
2025-03-19 19:07:26 -0500

    feat: setup about#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "about" => "about#index"
   resources :businesses
   resources :contractors
   resources :parks
@@ -14,5 +15,5 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "about#index"
 end
